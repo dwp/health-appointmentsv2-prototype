b3aa68d41665a1d4d86a6507dda1d4fe5cf8330b
Jen Rahman <jen.rahman@engineering.digital.dwp.gov.uk>
2023-10-06 12:08:09 +0100

    draft v3 set up

--- app/routes.js
+++ app/routes.js
@@ -265,3 +265,22 @@ router.post('/status-check', function (req, res) {
 
 })
 
+
+
+//v2
+router.post('/manage-what3', function (req, res) {
+
+  // Make a variable and give it the value from page
+  var manage = req.session.data['manage']
+
+  // Check whether the variable matches a condition
+  if (manage == "agents"){
+    // Send user to what language page
+    res.redirect('/v3/availability-management/agents/agent-profiles')
+  } else {
+    // Send user to next page
+    res.redirect('/v3/availability-management/availability/existing-slots')
+  }
+
+})
+
