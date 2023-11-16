06f4e2d4506d37e97b8dcf2d4a61a35f974ff1f4
Jen Rahman <jen.rahman@engineering.digital.dwp.gov.uk>
2023-11-16 10:29:57 +0000

    draft for v4 space

--- app/routes.js
+++ app/routes.js
@@ -284,3 +284,23 @@ router.post('/manage-what3', function (req, res) {
 
 })
 
+
+
+
+//v4
+router.post('/manage-what4', function (req, res) {
+
+  // Make a variable and give it the value from page
+  var manage = req.session.data['manage']
+
+  // Check whether the variable matches a condition
+  if (manage == "agents"){
+    // Send user to what language page
+    res.redirect('/v4/availability-management/agents/agent-profiles')
+  } else {
+    // Send user to next page
+    res.redirect('/v4/availability-management/availability/existing-slots')
+  }
+
+})
+
