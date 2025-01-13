49356b0075c4593e0a1060da0ba0333a1b0f97a7
Jen Rahman <jen.rahman@engineering.digital.dwp.gov.uk>
2025-01-13 11:21:14 +0000

    v8 with copy feature

--- app/routes.js
+++ app/routes.js
@@ -497,3 +497,80 @@ router.post('/select-hcp7-manage', function (req, res) {
   }
 
 })
+
+
+
+
+
+
+//v8
+router.post('/manage-what8', function (req, res) {
+
+  // Make a variable and give it the value from page
+  var manage = req.session.data['manage']
+
+  // Check whether the variable matches a condition
+  if (manage == "agents"){
+    // Send user to what language page
+    res.redirect('/v8/availability-management/agents/agent-profiles')
+  } else {
+    // Send user to next page
+    res.redirect('/v8/availability-management/availability/availability-view')
+  }
+
+})
+
+
+
+router.post('/status-check8', function (req, res) {
+
+  // Make a variable and give it the value from page
+  var status = req.session.data['status']
+
+  // Check whether the variable matches a condition
+  if (status == "assessment-booked"){
+    // Send user to what language page
+    res.redirect('/v8/book-appt/update-status-assessment-booked')
+  } else {
+    // Send user to next page
+    res.redirect('/v8/book-appt/update-status-confirmation')
+  }
+
+})
+
+
+
+
+router.post('/select-hcp8', function (req, res) {
+
+  // Make a variable and give it the value from page
+  var selectagent = req.session.data['select-agent']
+
+  // Check whether the variable matches a condition
+  if (selectagent == "choose-hcp"){
+    // Send user to what language page
+    res.redirect('/v8/book-appt/assign-hcp-while-booking')
+  } else {
+    // Send user to next page
+    res.redirect('/v8/book-appt/more-info')
+  }
+
+})
+
+
+
+router.post('/select-hcp8-manage', function (req, res) {
+
+  // Make a variable and give it the value from page
+  var selectagent = req.session.data['select-agent']
+
+  // Check whether the variable matches a condition
+  if (selectagent == "choose-hcp"){
+    // Send user to what language page
+    res.redirect('/v8/has-manage-appt/assign-hcp-while-booking')
+  } else {
+    // Send user to next page
+    res.redirect('/v8/has-manage-appt/appt-details-change-hcp')
+  }
+
+})
